7c9f6602ccbf2be662e5f8f083b320bf8c62b3ec
Moises <moisesteixeiradebrito@gmail.com>
2021-10-23 17:14:09 -0300

    First commit: testando js e responsividade

---
+++ scripts/scripts.js
@@ -0,0 +1,54 @@
+// Regras
+// Carne: 400 g para menos de 6 horas, 650 g acima;
+// Cerveja: 1200 ml para menos de 6 horas, 2000 ml acima;
+// Bebidas: 1000 ml para menos de 6 horas, 1500 ml acima;
+// Crianças: consomem metade dos adultos.
+// ________________________________________________________
+
+let inputAdultos = document.getElementById("adultos");
+let inputNaoBebe = document.getElementById("naobebe");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let naobebe = inputNaoBebe.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + carnePP(duracao) * naobebe + ((carnePP(duracao) / 2) * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + carnePP(duracao) * naobebe + ((bebidasPP(duracao) / 2) * criancas);
+    
+    resultado.innerHTML = `<p>${qdtTotalCarne/1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja/355)} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas/2000)} Pet's 2l de Bebidas</p>`
+}
+
+function carnePP(duracao){
+    if(duracao >= 6){
+        return 650;
+    }else{
+        return 400;
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000;
+    }else{
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao){
+    if(duracao >= 6){
+        return 1500;
+    }else{
+        return 1000;
+    }
+}
